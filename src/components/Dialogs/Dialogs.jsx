68a6e75f55f83e08554b7f34682277ac40217ee6
Andrey Sery <andrey.72006@gmail.com>
2023-01-28 00:59:42 +0300

    l-8

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import '../../App.css'
+import '../../App.css';
 import styles from './Dialogs.module.css';
 import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) => {
-  return <div className={`${styles.block} content` } >
-    <ul>
-      <li>
-        <NavLink to='/dialogs/1' className={(data)=> data.isActive ? styles.active : styles.item}>Dimych</NavLink>
-      </li>
-      <li>
-        <NavLink to='/dialogs/2' className={(data)=> data.isActive ? styles.active : styles.item}>Andrey</NavLink>
-      </li>
-      <li>
-        <NavLink to='/dialogs/3' className={(data)=> data.isActive ? styles.active : styles.item}>Sveta</NavLink>
-      </li>
-      <li>
-        <NavLink to='/dialogs/4' className={(data)=> data.isActive ? styles.active : styles.item}>Sasha</NavLink>
-      </li>
-      <li>
-        <NavLink to='/dialogs/5' className={(data)=> data.isActive ? styles.active : styles.item}>Victor</NavLink>
-      </li>
-      <li>
-        <NavLink to='/dialogs/6' className={(data)=> data.isActive ? styles.active : styles.item}>Valera</NavLink>
-      </li>
-      <li>
-        <NavLink to='/dialogs/7' className={(data)=> data.isActive ? styles.active : styles.item}>Dimych</NavLink>
-      </li>
-    </ul>
-    <div>
-      message
+  return (
+    <div className={`${styles.block} content`}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <NavLink to="/dialogs/1">Dimych</NavLink>
+        </li>
+        <li className={styles.item}>
+          <NavLink to="/dialogs/2">Andrey</NavLink>
+        </li>
+        <li className={styles.item}>
+          <NavLink to="/dialogs/3">Sveta</NavLink>
+        </li>
+        <li className={styles.item}>
+          <NavLink to="/dialogs/4">Sasha</NavLink>
+        </li>
+        <li className={styles.item}>
+          <NavLink to="/dialogs/5">Victor</NavLink>
+        </li>
+        <li className={styles.item}>
+          <NavLink to="/dialogs/6">Valera</NavLink>
+        </li>
+        <li className={styles.item}>
+          <NavLink to="/dialogs/7">Dimych</NavLink>
+        </li>
+      </ul>
+      <div className={styles.messageText}>message</div>
     </div>
-  </div>;
+  );
 };
 
 export default Dialogs;
