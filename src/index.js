ef8254b3ebba974c9fa21940490791bb7f03ac99
Andrey Sery <andrey.72006@gmail.com>
2023-02-06 23:28:37 +0300

    Creating empty project

--- src/index.js
+++ src/index.js
@@ -5,16 +5,10 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let rerenderEntireTree = (state) => {
-  root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App state={state} dispatch={store.dispatch.bind(store)} />
-      </BrowserRouter>
-    </React.StrictMode>,
-  );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(rerenderEntireTree);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App state={state} dispatch={store.dispatch.bind(store)} />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
