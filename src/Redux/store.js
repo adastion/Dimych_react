b6f20312bb3a09da9a9dc25f9c9ce25d6a1b7e36
Andrey Sery <andrey.72006@gmail.com>
2023-02-08 23:01:23 +0300

    Store, state

---
+++ src/Redux/store.js
@@ -0,0 +1,27 @@
+const stor = {
+  state: {
+    profilePage: {
+      past: [
+        { id: 1, message: 'Hi', likesCount: 1 },
+        { id: 2, message: 'Hi', likesCount: 0 },
+        { id: 3, message: 'Hi', likesCount: 12 },
+        { id: 4, message: 'Hi', likesCount: 2 },
+      ],
+
+      newPostText: 'textarea',
+    },
+
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Andrey' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Victor' },
+        { id: 6, name: 'Valera' },
+      ],
+    },
+
+    sidebar: {}
+  },
+};
