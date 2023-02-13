9a3cac577c13512f6f90f70e3ff2e5e277778bbe
Andrey Sery <andrey.72006@gmail.com>
2023-02-13 23:03:23 +0300

    Refactoring to switch

--- src/Redux/dialogs-reducer.js
+++ src/Redux/dialogs-reducer.js
@@ -2,12 +2,16 @@ const ADD_MESSAGE = 'ADD-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 const dialogsReducer = (state, action) => {
-  if (action.type === ADD_MESSAGE) {
-    let newMessage = { id: 1, message: state.newMessageText, name: 'Dimych' };
-    state.dialogs.push(newMessage);
-    state.newMessageText = '';
-  } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-    state.newMessageText = action.newText;
+  switch (action.type) {
+    case ADD_MESSAGE:
+      let newMessage = { id: 1, message: state.newMessageText, name: 'Dimych' };
+      state.dialogs.push(newMessage);
+      state.newMessageText = '';
+      return state;
+
+    case UPDATE_NEW_MESSAGE_TEXT:
+      state.newMessageText = action.newText;
+      return state;
   }
   return state;
 };
--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -2,16 +2,20 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_CHANGE = 'UPDATE-NEW-POST-CHANGE';
 
 const profileReducer = (state, action) => {
-  if (action.type === ADD_POST) {
-    let newPost = {
-      id: 1,
-      message: state.newPostText,
-      likesCount: 0,
-    };
-    state.posts.push(newPost);
-    state.newPostText = '';
-  } else if (action.type === UPDATE_NEW_POST_CHANGE) {
-    state.newPostText = action.newText;
+  switch (action.type) {
+    case ADD_POST:
+      let newPost = {
+        id: 1,
+        message: state.newPostText,
+        likesCount: 0,
+      };
+      state.posts.push(newPost);
+      state.newPostText = '';
+      return state;
+
+    case UPDATE_NEW_POST_CHANGE:
+      state.newPostText = action.newText;
+      return state;
   }
   return state;
 };
